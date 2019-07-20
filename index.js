337dceef0a33d184dbf0b833f7b58ab739d9b200
Anton Bortnikov <6088046+ntnbrtnkv@users.noreply.github.com>
2019-07-20 18:28:43 +0500

    combineReducers redux

--- index.js
+++ index.js
@@ -1,55 +1,65 @@
-// import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 
-function createStore(reducer, enhancer) {
-  let state;
-  let listeners = [];
+// function createStore(reducer, enhancer) {
+//   let state;
+//   let listeners = [];
 
-  if (typeof enhancer !== 'undefined') {
-    return enhancer(createStore)(reducer);
+//   if (typeof enhancer !== 'undefined') {
+//     return enhancer(createStore)(reducer);
+//   }
+  
+//   const getState = () => state;
+  
+//   const subscribe = (cb) => {
+//     listeners.push(cb);
+//     return () => {
+//       const index = listeners.indexOf(cb);
+//       listeners.splice(index, 1);
+//     }
+//   };
+  
+//   const dispatch = (action) => {
+//     state = reducer(state, action);
+//     listeners.forEach(cb => cb());
+//   };
+  
+//   dispatch({type: '@@myredux/INIT'});
+  
+//   return {
+//     getState,
+//     subscribe,
+//     dispatch
+//   };
+// }
+
+// const applyMiddleware = (...middlewares) => {
+//   return createStore => (...args) => {
+//     const store = createStore(...args);
+
+//     const middlewareAPI = {
+//       getState: store.getState
+//     };
+
+//     const chain = middlewares.map(m => m(middlewareAPI));
+//     const compose = (...funcs) => funcs.reduce((a, b) => (...args) => a(b(...args)));
+//     const dispatch = compose(...chain)(store.dispatch);
+
+//     return {
+//       ...store,
+//       dispatch
+//     }
+//   }
+// }
+
+const clicks = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+    case "DECREMENT":
+      return state + 1;
+    default:
+      return state;
   }
-  
-  const getState = () => state;
-  
-  const subscribe = (cb) => {
-    listeners.push(cb);
-    return () => {
-      const index = listeners.indexOf(cb);
-      listeners.splice(index, 1);
-    }
-  };
-  
-  const dispatch = (action) => {
-    state = reducer(state, action);
-    listeners.forEach(cb => cb());
-  };
-  
-  dispatch({type: '@@myredux/INIT'});
-  
-  return {
-    getState,
-    subscribe,
-    dispatch
-  };
-}
-
-const applyMiddleware = (...middlewares) => {
-  return createStore => (...args) => {
-    const store = createStore(...args);
-
-    const middlewareAPI = {
-      getState: store.getState
-    };
-
-    const chain = middlewares.map(m => m(middlewareAPI));
-    const compose = (...funcs) => funcs.reduce((a, b) => (...args) => a(b(...args)));
-    const dispatch = compose(...chain)(store.dispatch);
-
-    return {
-      ...store,
-      dispatch
-    }
-  }
-}
+};
 
 const counter = (state = 0, action) => {
   switch (action.type) {
@@ -68,11 +78,16 @@ const logger = store => next => action => {
   console.log('next state', store.getState());
   return result;
 }
+const rootStore = combineReducers({
+  counter,
+  clicks
+});
 
-const store = createStore(counter, applyMiddleware(logger));
+const store = createStore(rootStore, applyMiddleware(logger));
 
 const render = () => {
-  document.getElementById("counter").innerText = store.getState();
+  document.getElementById("counter").innerText = store.getState().counter;
+  document.getElementById("clicks").innerText = store.getState().clicks;
 };
 
 window.unsubscribe = store.subscribe(render);
