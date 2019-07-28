3a497648be9bc93a2b60f7a929bbad324d0e2bb3
Anton Bortnikov <6088046+ntnbrtnkv@users.noreply.github.com>
2019-07-28 17:52:48 +0500

    feat: implement box and autorun

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
-import { observable, autorun } from "mobx";
-const box = observable.box;
+// mobx.js
+
+let currentRunning;
+
+function box(initial) {
+  let value = initial;
+  return {
+    observers: [],
+    get() {
+      if (currentRunning) {
+        currentRunning.observing.push(this);
+      }
+      return value;
+    },
+    set(v) {
+      value = v;
+      this.observers.splice(0).forEach(r => r.run());
+    }
+  }
+}
+
+function autorun(cb) {
+  const reaction = {
+    observing: [],
+    run() {
+      currentRunning = this;
+      this.observing = [];
+      cb();
+      this.observing.forEach(box => box.observers.push(this));
+      currentRunning = undefined;
+
+    }
+  }
+  reaction.run();
+}
+
+// index.js
 
 const todo = box("Learn MobX");
 const done = box(false);
